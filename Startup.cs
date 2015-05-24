c5b59c5bf1a1488349093ea9869c4a6f492056fa
annwang <annwangca@yahoo.ca>
2015-05-24 03:47:25 -0400

    One ASP.NET MVC 5 Project

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(YWPatient.Startup))]
+namespace YWPatient
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
